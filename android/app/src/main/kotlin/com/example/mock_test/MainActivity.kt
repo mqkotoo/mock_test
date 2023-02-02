7f97970d2859d08edf0820a98e162af606d6b5a4
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-02-02 20:34:05 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/mock_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mock_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    final repository = GitHubApiRepository();
+    repository.countRepositories().then((result) {
+      setState(() {
+        _counter = result;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class GitHubApiRepository {
+  static const String kApiUrl =
+      "https://api.github.com/search/repositories?q=flutter";
+
+  Future<int> countRepositories() async {
+    final http.Client client = http.Client();
+    final response = await client.get(Uri.parse(kApiUrl));
+    final map = json.decode(response.body) as Map<String, dynamic>;
+
+    return map['total_count'] ?? -1;
+  }
+}
